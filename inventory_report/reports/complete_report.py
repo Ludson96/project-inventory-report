6b395467f5dc1cdf45fd3dbb280478b4b6ff10ca
Ludson96 <ludson_ps27@hotmail.com>
2023-04-11 17:49:37 -0300

    refactor: :recycle: simple_report methods

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,11 @@
-class CompleteReport:
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
     pass
+
+
+#     @staticmethod
+#     def generate(items: list[dict]) -> str:
+#         simple_report = SimpleReport.generate(items)
+#         print("🚀 ~ file: complete_report.py:8 ~ simple_report:", simple_report)
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,33 +1,36 @@
 from datetime import datetime
+from collections import Counter
 
 
 class SimpleReport:
-    @staticmethod
-    def generate(items):
+    @classmethod
+    def get_oldest_fabricacao(cls, items: list[dict]) -> str:
         datas_de_fabricacao = [
             datetime.strptime(item["data_de_fabricacao"], "%Y-%m-%d").date()
             for item in items
         ]
-        oldest_fabricacao = min(datas_de_fabricacao)
+        return min(datas_de_fabricacao)
 
+    @classmethod
+    def get_newest_validade(cls, items: list[dict]) -> str:
         datas_de_validade = [
             datetime.strptime(item["data_de_validade"], "%Y-%m-%d").date()
             for item in items
             if datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
             > datetime.now()
         ]
-        newest_de_validade = min(datas_de_validade)
+        return min(datas_de_validade)
 
-        empresas = {}
-        for item in items:
-            empresa = item["nome_da_empresa"]
-            if empresa in empresas:
-                empresas[empresa] += 1
-            else:
-                empresas[empresa] = 1
+    @classmethod
+    def quantity_product_empresas(cls, items: list[dict]) -> str:
+        empresas = [item["nome_da_empresa"] for item in items]
+        return Counter(empresas).most_common(1)[0][0]
 
-        empresa_mais_frequente = max(empresas, key=empresas.get)
-
+    @classmethod
+    def generate(cls, items: list[dict]) -> str:
+        newest_de_validade = cls.get_newest_validade(items)
+        oldest_fabricacao = cls.get_oldest_fabricacao(items)
+        empresa_mais_frequente = cls.quantity_product_empresas(items)
         return (
             f"Data de fabricação mais antiga: {oldest_fabricacao}\n"
             f"Data de validade mais próxima: {newest_de_validade}\n"
