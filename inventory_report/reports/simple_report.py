9c3cb80227b6ae0e0bf43aac4a8cbc97fbf2461e
Ludson96 <ludson_ps27@hotmail.com>
2023-04-11 14:48:00 -0300

    feat: :sparkles: add function generate
    Generate function responsible for returning the oldest manufacturing date, the closest expiry date and the company with the most products.

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,35 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(items):
+        datas_de_fabricacao = [
+            datetime.strptime(item["data_de_fabricacao"], "%Y-%m-%d").date()
+            for item in items
+        ]
+        oldest_fabricacao = min(datas_de_fabricacao)
+
+        datas_de_validade = [
+            datetime.strptime(item["data_de_validade"], "%Y-%m-%d").date()
+            for item in items
+            if datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
+            > datetime.now()
+        ]
+        newest_de_validade = min(datas_de_validade)
+
+        empresas = {}
+        for item in items:
+            empresa = item["nome_da_empresa"]
+            if empresa in empresas:
+                empresas[empresa] += 1
+            else:
+                empresas[empresa] = 1
+
+        empresa_mais_frequente = max(empresas, key=empresas.get)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_fabricacao}\n"
+            f"Data de validade mais próxima: {newest_de_validade}\n"
+            f"Empresa com mais produtos: {empresa_mais_frequente}"
+        )
