318b6f0482d228056c3444201fc7f5372388010c
Ludson96 <ludson_ps27@hotmail.com>
2023-04-11 11:34:59 -0300

    test: :white_check_mark: add unit test for Class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,12 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(1, "trybe", "eu", "26-09", "26-09", "123456", "aaaa")
+    assert produto.id == 1
+    assert produto.nome_da_empresa == "eu"
+    assert produto.nome_do_produto == "trybe"
+    assert produto.data_de_fabricacao == "26-09"
+    assert produto.data_de_validade == "26-09"
+    assert produto.numero_de_serie == "123456"
+    assert produto.instrucoes_de_armazenamento == "aaaa"
