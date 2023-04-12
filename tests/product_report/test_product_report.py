bf066f3d7c2e4ce142643ac1e15fd331ca119587
Ludson96 <ludson_ps27@hotmail.com>
2023-04-12 11:16:49 -0300

    test: :white_check_mark: add teste para product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "123",
+        "ao abrigo de luz",
+    )
+
+    frase = "O produto farinha fabricado em 01-05-2021 por Farinini com validade até 02-06-2023 precisa ser armazenado ao abrigo de luz."
+    assert frase == str(product)
