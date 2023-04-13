061991ffea08e08a335d3c5517de3d23b7649fa5
Ludson96 <ludson_ps27@hotmail.com>
2023-04-12 21:58:57 -0300

    feat: :sparkles: add class main

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,5 +1,7 @@
 from collections.abc import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
@@ -11,6 +13,11 @@ class InventoryRefactor(Iterable):
         data = self.importer.import_data(path)
         for item in data:
             self.data.append(item)
+
+        if report_type == "simples":
+            return SimpleReport.generate(data)
+        elif report_type == "completo":
+            return CompleteReport.generate(data)
 
     def __iter__(self) -> list[dict]:
         return InventoryIterator(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) <= 2:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    importers = {
+        "csv": CsvImporter,
+        "json": JsonImporter,
+        "xml": XmlImporter,
+    }
+
+    _, path, report_type = sys.argv
+
+    file_extension = path.split(".")[-1]
+    report = InventoryRefactor(importers[file_extension])
+    reader = report.import_data(path, report_type)
+
+    print(reader, end="")
